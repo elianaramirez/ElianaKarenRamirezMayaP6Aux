372aaf56bb30288f024a13f24a0753955507899b
elianaramirez <elianarami22@gmail.com>
2021-10-26 05:47:04 +0200

    laboratorioseiss

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Movies } from "./Movies";
+
+export function App(){
+    return (
+    <div>
+        <header>
+            <h1>Peliculas- Star Wars</h1>
+        </header>
+        <main>
+          <Movies />
+        </main>
+    </div>
+ );
+}
---
+++ src/Movies.js
@@ -0,0 +1,16 @@
+import movies from "./movies.json";
+import axios from 'axios';
+console.log(movies);
+
+
+export function Movies()  {
+    
+  componentDidMount() 
+  axios.get()
+  .then(result => {
+   console.log(result)
+    })
+    .catch(console.log)
+  };
+       
+    
